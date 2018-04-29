c8d7d7c4ca01de3b37a294a7d9b51db85df5c4dc
lehotzky.erica@gmail.com <lehotzky.erica@gmail.com>
2018-04-29 15:30:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var string;
-var uppercase = 'HELLO!';
-var lowercase = 'hello!';
+var uppercase = "HELLO!";
+var lowercase = "hello!";
 uppercase.toUpperCase() === uppercase;
 lowercase.toLowerCase() === lowercase;
 
@@ -21,9 +21,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (lowercase.toLowerCase(string) === lowercase) {
+  if (string === lowercase) {
     return "I can't hear you!";
-  } else if (uppercase.toUpperCase(string) === uppercase) {
+  } else if (string === uppercase) {
       return "YES INDEED!"; 
     } else if (string === "I love you, Grandma.") {
       return "I love you, too.";
